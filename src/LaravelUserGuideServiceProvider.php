b6c1e24ac859234aa013a8ddfb100bfe806fac13
Yeasir Arafat <yeasirarafat321@gmail.com>
2021-11-21 17:13:07 +0600

    Update LaravelUserGuideServiceProvider.php

--- src/LaravelUserGuideServiceProvider.php
+++ src/LaravelUserGuideServiceProvider.php
@@ -9,8 +9,8 @@ class LaravelUserGuideServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->publishes([
-            __DIR__ . '/../config/user_guide.php' => config_path('user_guide.php')
-        ], 'user-guide');
+            __DIR__ . '/../config/user_guide.php' => config_path('user_guide.php'), 'user-guide'
+        ]);
 
         $this->loadRoutesFrom(__DIR__ . '/../routes/api.php');
         $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
